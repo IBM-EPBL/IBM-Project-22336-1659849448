db68eeeb7cf7151bd5ef2f5d6a7be48792cf9df8
abinivesh01 <102317504+abinivesh01@users.noreply.github.com>
2022-09-26 22:59:34 +0530

    Delete IBM Assignment2.py

--- IBM Assignment2.py
+++
@@ -1,9 +0,0 @@
-import random
-def check(temperature,humidity):
-    if(humidity<25):
-        if(temperature>25):
-            print("Temperature : ",temperature,"***Beep***")
-for i in range(0,20):
-    temperature = random.randint(10,100)
-    humidity = random.randint(10,100)
-    check(temperature,humidity)
